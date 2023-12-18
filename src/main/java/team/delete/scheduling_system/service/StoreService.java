316a53e903899a69444dd24c43564482097f13e7
Devin100086 <116065959+Devin100086@users.noreply.github.com>
2023-12-18 21:55:41 +0800

    fix: fix some bug of StoreService

--- src/main/java/team/delete/scheduling_system/service/StoreService.java
+++ src/main/java/team/delete/scheduling_system/service/StoreService.java
@@ -25,37 +25,37 @@ public class StoreService {
 
     /**
      * 判断权限
+     *
      * @param user 需要判断权限的用户对象
      */
-    public void judgePermission(User user){
+    public void judgePermission(User user) {
         if (user == null) {
             throw new AppException(ErrorCode.PARAM_ERROR);
         }
-        if(user.getType()!=User.Type.SUPER_ADMIN){
+        if (user.getType() != User.Type.SUPER_ADMIN) {
             throw new AppException(ErrorCode.USER_PERMISSION_ERROR);
         }
     }
 
     /**
      * 查询所有门店
+     *
      * @param user 操作的用户
      * @return 门店信息列表
      */
-    public List<Store> fetchAllStore(User user){
-        if (user == null) {
-            throw new AppException(ErrorCode.PARAM_ERROR);
-        }
+    public List<Store> fetchAllStore(User user) {
         judgePermission(user);
         return storeMapper.selectList(null);
     }
 
     /**
      * 增加门店信息
-     * @param user 操作的用户
+     *
+     * @param user  操作的用户
      * @param store 新增的门店对象
      */
-    public void addStore(User user,Store store){
-        if(store==null || user ==null){
+    public void addStore(User user, Store store) {
+        if (store == null) {
             throw new AppException(ErrorCode.PARAM_ERROR);
         }
         judgePermission(user);
@@ -64,11 +64,12 @@ public class StoreService {
 
     /**
      * 删除门店信息
-     * @param user 操作的用户
+     *
+     * @param user    操作的用户
      * @param storeId 新增的门店对象id
      */
-    public void deleteStore(User user,Integer storeId){
-        if(storeId==null || user ==null){
+    public void deleteStore(User user, Integer storeId) {
+        if (storeId == null) {
             throw new AppException(ErrorCode.PARAM_ERROR);
         }
         judgePermission(user);
@@ -77,11 +78,12 @@ public class StoreService {
 
     /**
      * 更新商店信息
-     * @param user 操作的用户
+     *
+     * @param user  操作的用户
      * @param store 新增的门店对象
      */
-    public void updateStore(User user,Store store){
-        if(store==null || user ==null){
+    public void updateStore(User user, Store store) {
+        if (store == null) {
             throw new AppException(ErrorCode.PARAM_ERROR);
         }
         judgePermission(user);
