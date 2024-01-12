3b81a4f2c33213e01287221409587001b7afe872
PatrickStar <1592810043@qq.com>
2024-01-12 16:34:38 +0800

    fix: fix some bug

--- src/main/java/team/delete/scheduling_system/controller/MessageController.java
+++ src/main/java/team/delete/scheduling_system/controller/MessageController.java
@@ -20,8 +20,15 @@ public class MessageController {
     final MessageService messageService;
 
     @ResponseBody
-    @GetMapping()
+    @GetMapping
     public Object fetchAllMessages() {
         return AjaxResult.SUCCESS(messageService.getMessage(StpUtil.getLoginIdAsInt()));
+    }
+
+    @ResponseBody
+    @PostMapping
+    public Object addMessage(@RequestParam(value = "user_id") Integer userId, @RequestParam String content) {
+        messageService.sendMessage(userId, content);
+        return AjaxResult.SUCCESS();
     }
 }
