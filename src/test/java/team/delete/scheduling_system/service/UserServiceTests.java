a7a5045080c936579db6d6f091b6457c93e39ecf
cookie1551 <202105720212@zjut.edu.cn>
2024-01-19 14:37:49 +0800

    feat: update the Unit Test

--- src/test/java/team/delete/scheduling_system/service/UserServiceTests.java
+++ src/test/java/team/delete/scheduling_system/service/UserServiceTests.java
@@ -45,24 +45,130 @@ public class UserServiceTests {
     }
 
     @Test
-    public void testFetchUserByUserId() {
+    public void testFetchUserByUserIdWithNullUser() {
+        AppException exception = assertThrows(AppException.class, () -> {
+            UserDto userDto = userService.fetchUserDtoByUserId(null);
+        });
+        assertEquals(ErrorCode.PARAM_ERROR, exception.getCode());
+    }
+    @Test
+    public void testFetchUserByUserIdWithOk() {
         Integer userId = 1;
         UserDto userDto = userService.fetchUserDtoByUserId(userId);
-        System.out.println(userDto);
         assertEquals(userId, userDto.getUserId());
     }
 
+
     @Test
-    public void testUpdateUser() {
+    public void testUpdateUserWithNullUserId () {
+        AppException exception = assertThrows(AppException.class, () -> {
+            userService.updateUser(null,
+                    User.builder()
+                            .userId(15)
+                            .name("customer_service1")
+                            .storeId(1)
+                            .groupId(4)
+                            .type(User.Type.CUSTOMER_SERVICE).build());
+        });
+        assertEquals(ErrorCode.PARAM_ERROR, exception.getCode());
+    }
+    @Test
+    public void testUpdateUserWithNullTypeArea  () {
+        AppException exception = assertThrows(AppException.class, () -> {
+            userService.updateUser(1,null);
+        });
+        assertEquals(ErrorCode.PARAM_ERROR, exception.getCode());
+    }
+    @Test
+    public void testUpdateUserWithErrorTypeUser () {
+        AppException exception = assertThrows(AppException.class, () -> {
+            userService.updateUser(1,
+                    User.builder()
+                            .userId(55)
+                            .name("worker").build());
+        });
+        assertEquals(ErrorCode.USER_NOT_EXISTED, exception.getCode());
+    }
+    @Test
+    public void testUpdateUserWithOkAreaSys() {
         userService.updateUser(1,
                 User.builder()
-                        .userId(5)
-                        .name("王五")
-                        .password("12345678")
+                        .userId(15)
+                        .name("customer_service1")
                         .storeId(1)
-                        .groupId(1)
+                        .groupId(4)
                         .type(User.Type.CUSTOMER_SERVICE).build());
     }
+    @Test
+    public void testUpdateUserWithOkArea1() {
+        userService.updateUser(3,
+                User.builder()
+                        .userId(15)
+                        .name("customer_service1")
+                        .storeId(1)
+                        .groupId(4)
+                        .type(User.Type.CUSTOMER_SERVICE).build());
+    }
+    @Test
+    public void testUpdateUserWithErrorArea1() {
+        AppException exception = assertThrows(AppException.class, () -> {
+            userService.updateUser(33,
+                    User.builder()
+                            .userId(15)
+                            .name("customer_service1")
+                            .storeId(1)
+                            .groupId(4)
+                            .type(User.Type.CUSTOMER_SERVICE).build());
+        });
+        assertEquals(ErrorCode.USER_PERMISSION_ERROR, exception.getCode());
+    }
+
+    @Test
+    public void testUpdateUserWithOkArea2() {
+        userService.updateUser(3,
+                User.builder()
+                        .userId(15)
+                        .name("customer_service1")
+                        .storeId(1)
+                        .groupId(4)
+                        .type(User.Type.CUSTOMER_SERVICE).build());
+    }
+    @Test
+    public void testUpdateUserWithErrorArea2() {
+        AppException exception = assertThrows(AppException.class, () -> {
+            userService.updateUser(4,
+                    User.builder()
+                            .userId(2)
+                            .name("cashier1")
+                            .storeId(1)
+                            .groupId(4)
+                            .type(User.Type.CUSTOMER_SERVICE).build());
+        });
+        assertEquals(ErrorCode.USER_PERMISSION_ERROR, exception.getCode());
+    }
+
+    @Test
+    public void testUpdateUserWithOkArea3() {
+        userService.updateUser(5,
+                User.builder()
+                        .userId(15)
+                        .name("customer_service1")
+                        .storeId(1)
+                        .groupId(4).build());
+    }
+    @Test
+    public void testUpdateUserWithErrorArea3() {
+        AppException exception = assertThrows(AppException.class, () -> {
+            userService.updateUser(5,
+                    User.builder()
+                            .userId(21)
+                            .name("customer_service5")
+                            .storeId(1)
+                            .groupId(5).build());
+        });
+        assertEquals(ErrorCode.USER_PERMISSION_ERROR, exception.getCode());
+    }
+
 
     @Test
     public void testChangePasswordSuccess() {
@@ -77,10 +183,39 @@ public class UserServiceTests {
         assertEquals(User.Type.SUPER_ADMIN, userService.login("Admin", "Admin123"));
     }
 
+
     @Test
-    public void testFetchAllUser() {
+    public void testFetchAllUserWithNullUserId () {
+        AppException exception = assertThrows(AppException.class, () -> {
+            userService.fetchAllUser(null, 1, 1);
+        });
+        assertEquals(ErrorCode.PARAM_ERROR, exception.getCode());
+    }
+    @Test
+    public void testFetchAllUserWithNullTypeArea  () {
+        AppException exception = assertThrows(AppException.class, () -> {
+            userService.fetchAllUser(1, null, 1);
+        });
+        assertEquals(ErrorCode.PARAM_ERROR, exception.getCode());
+    }
+    @Test
+    public void testFetchAllUserWithNullTypeId () {
+        AppException exception = assertThrows(AppException.class, () -> {
+            userService.fetchAllUser(1, 1, null);
+        });
+        assertEquals(ErrorCode.PARAM_ERROR, exception.getCode());
+    }
+    @Test
+    public void testFetchAllUserWithErrorType () {
+        AppException exception = assertThrows(AppException.class, () -> {
+            userService.fetchAllUser(2, 1, 1);
+        });
+        assertEquals(ErrorCode.USER_PERMISSION_ERROR, exception.getCode());
+    }
+    @Test
+    public void testFetchAllUserWithOkAreaSys() {
         Integer userId = 1;
-        Integer typeArea = 3;
+        Integer typeArea = 1;
         Integer typeId = 1;
         List<User> userList = userService.fetchAllUser(userId, typeArea, typeId);
         switch (typeArea) {
@@ -101,6 +236,70 @@ public class UserServiceTests {
                 break;
         }
     }
+    @Test
+    public void testFetchAllUserWithOkArea1() {
+        Integer userId = 3;
+        Integer typeArea = 1;
+        Integer typeId = 1;
+        List<User> userList = userService.fetchAllUser(userId, typeArea, typeId);
+        userList.forEach(user -> {
+            assertEquals(typeId, user.getStoreId());
+        });
+    }
+    @Test
+    public void testFetchAllUserWithErrorArea1() {
+        Integer userId = 3;
+        Integer typeArea = 1;
+        Integer typeId = 2;
+        AppException exception = assertThrows(AppException.class, () -> {
+            userService.fetchAllUser(userId, typeArea, typeId);
+        });
+        assertEquals(ErrorCode.USER_PERMISSION_ERROR, exception.getCode());
+    }
+
+    @Test
+    public void testFetchAllUserWithOkArea2() {
+        Integer userId = 4;
+        Integer typeArea = 2;
+        Integer typeId = 3;
+        List<User> userList = userService.fetchAllUser(userId, typeArea, typeId);
+        userList.forEach(user -> {
+            assertEquals(User.Type.CUSTOMER_SERVICE, user.getType());
+        });
+    }
+    @Test
+    public void testFetchAllUserWithErrorArea2() {
+        Integer userId = 4;
+        Integer typeArea = 2;
+        Integer typeId = 4;
+        AppException exception = assertThrows(AppException.class, () -> {
+            userService.fetchAllUser(userId, typeArea, typeId);
+        });
+        assertEquals(ErrorCode.USER_PERMISSION_ERROR, exception.getCode());
+    }
+
+    @Test
+    public void testFetchAllUserWithOkArea3() {
+        Integer userId = 5;
+        Integer typeArea = 3;
+        Integer typeId = 4;
+        List<User> userList = userService.fetchAllUser(userId, typeArea, typeId);
+        userList.forEach(user -> {
+            assertEquals(typeId, user.getGroupId());
+        });
+    }
+    @Test
+    public void testFetchAllUserWithErrorArea3() {
+        Integer userId = 5;
+        Integer typeArea = 3;
+        Integer typeId = 5;
+        AppException exception = assertThrows(AppException.class, () -> {
+            userService.fetchAllUser(userId, typeArea, typeId);
+        });
+        assertEquals(ErrorCode.USER_PERMISSION_ERROR, exception.getCode());
+    }
+
+
 
     @Test
     public void testFetchUserByGroup() {
